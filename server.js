883b63175490000419a149f932d14fc5a263fe61
Eduárd Pocstár <eduard.pocstar@admdh.com>
2016-04-08 16:39:43 +0200

    server.js updates

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-var express = require('express');
-var morgan = require('morgan');
-var server  = express();
-var path = require('path');
-var open = require("open");
-var pjson = require('./package.json');
-var myipui = require('my-ip-ui');
-var timeStamp = require("console-stamp");
+var express           = require('express');
+var morgan            = require('morgan');
+var server            = express();
+var path              = require('path');
+var open              = require("open");
+var pjson             = require('./package.json');
+var myipui            = require('my-ip-ui');
+var timeStamp         = require("console-stamp");
+var dateFormat        = require('dateformat');
 
 
-var src = '/webroot';
-var pref = 'http://';
-var host = pjson.host || 'adm-dev-kit';
-var port = pjson.port || 3080;
+var src               = '/webroot';
+var pref              = 'http://';
+var host              = pjson.host || 'adm-dev-kit';
+var port              = pjson.port || 3080;
 
 
 // PostCSS PLUGINS
@@ -22,7 +23,6 @@ var vars			  = require('postcss-simple-vars');
 var minmax		      = require('postcss-media-minmax');
 var mscale		      = require('postcss-modular-scale');
 var grid			  = require('postcss-simple-grid');
-
 
 // PostCSS SETTINGS
 var cssVariables = '.' + src + '/cssVariables.js';
@@ -48,26 +48,24 @@ var postcssPlugins = [
 
 // SEVER
 server.use(myipui({ port: port }));
+
+// SERVER STATIC
 server.use(express.static(path.join(__dirname, src)));
 server.set('views', path.join(__dirname, src));
 server.set('view engine', 'jade');
 server.set('view cache', false);
 
 
-// var date = new Date();
-// var now = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
-// var dateFormat(now, "dddd, mmmm dS, yyyy, h:MM:ss TT");
-// dateFormat.masks.hammerTime = 'HH:MM:SS';
-// now.format("hammerTime");
+// TIMESTAMP
+function getTime(){
+	var now = new Date();
+	var time = '[' + dateFormat(now, "hh:MM:ss") + ']';
+	return time;
+}
 
-var dateFormat = require('dateformat');
-var now = new Date();
-
-var time = '[' + dateFormat(now, "hh:MM:ss") + ']';
-
-morgan.token("timeStamp", function (req, res) { return time });
-
-server.use(morgan(':timeStamp :method :url :status :response-time ms - :res[content-length]'));
+// LOG
+morgan.token("timeStamp", function (req, res) { return getTime() });
+server.use(morgan(':timeStamp :method :status :url :response-time ms'));
 
 server.get('/', function (req, res) {
 	res.render('index');
@@ -75,6 +73,7 @@ server.get('/', function (req, res) {
 
 server.get('/:pageUrl', function (req, res) {
 	var pageUrl = req.params.pageUrl;
+	
 	res.render(pageUrl);
 });
 
@@ -84,25 +83,24 @@ server.use('/webroot/*.css', postcssMiddleware({
 		return path.join(__dirname, req.originalUrl);
 	},
 	plugins: postcssPlugins
-
 }));
 
 
-// timeStamp(console, {
-// 	pattern : "HH:MM:ss",
-// 	label: false
-// });
+timeStamp(console, {
+	pattern : "HH:MM:ss",
+	label: false
+});
 
 
 // START SERVER ON DEFINED PORT
 server.listen(port, function(res, req){
-	console.log(time + '');
-	console.log(time + '');
-	console.log(time + 'Server started!');
-	console.log(time + '-------------------------------------------------------');
-	console.log(time + 'HTTP Server        : ' + pref + host + ':' + port);
-	console.log(time + '-------------------------------------------------------');
-	console.log(time + '');
-	console.log(time + '');
+	console.log('');
+	console.log('');
+	console.log('Server started!');
+	console.log('-------------------------------------------------------');
+	console.log('HTTP Server        : ' + pref + host + ':' + port);
+	console.log('-------------------------------------------------------');
+	console.log('');
+	console.log('');
 	open(pref + host + ':' + port);
 });
